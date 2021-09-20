79b3968e78cccdeb500a7c97eab8029f83bd6a95
Вадим <v.dudko93@gmail.com>
2021-09-20 17:22:41 +0300

    feat: add hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const MENU = document.getElementById('menu');
+
+MENU.addEventListener('click', event => {
+    MENU.querySelectorAll('li').forEach(element => element.classList.remove('nav__item__link--active'));
+    event.target.parentNode.classList.add('nav__item__link--active');
+});
+
+MENU.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        event.preventDefault();
+
+        document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener('scroll', onScroll);
+
+function onScroll(event){
+    const CURRENT = window.scrollY;
+    const SECTIONS = document.querySelectorAll('section');
+    const MENU_LIST = document.querySelectorAll('.nav__item__link');
+
+    SECTIONS.forEach((element) => {
+        if(element.offsetTop - 89 <= CURRENT && (element.offsetTop + element.offsetHeight - 89) > CURRENT){
+            MENU_LIST.forEach((li) => {
+                li.classList.remove('nav__item__link--active');
+                if(element.getAttribute('id') === li.querySelector('.nav__item__link a').getAttribute('href').substring(1)){
+                    li.classList.add('nav__item__link--active');
+                }
+            });
+        }
+    });
+}
+
+const HAMBURGER_MENU = document.querySelector('.hamburger');
+const HAMBURGER_NAV = document.querySelector('.nav__item');
+const ff = document.querySelector('section');
+
+HAMBURGER_MENU.addEventListener('click', () => {
+    HAMBURGER_MENU.classList.toggle('hamburger__rotate--active');
+    HAMBURGER_NAV.classList.toggle('hamburger--active');
+});
+
+document.addEventListener('click', (event) => {
+    if (event.target.closest('.nav__item')) {
+        HAMBURGER_MENU.classList.remove('hamburger__rotate--active');
+        HAMBURGER_NAV.classList.remove('hamburger--active');
+    }
+})
+
+
 console.log(`
 
 score: 140 - 150
